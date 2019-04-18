0c962f6c4360667ae10b6fea720a99c155dfcd36
skipness <skipness123@gmail.com>
2019-04-18 17:09:50 +0800

    Add method to generate, read and delete device id

--- lib/repository/me.dart
+++ lib/repository/me.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:meta/meta.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:lihkg_flutter/networking/api_client.dart';
@@ -10,7 +11,9 @@ class MeRepository {
     @required String email,
     @required String password,
   }) async {
-    final result = await ApiClient().login(email, password);
+    await persistDeviceId();
+    final result =
+        await ApiClient(deviceId: await readDeviceId()).login(email, password);
     if (result.errorCode != null) {
       throw result.errorMessage;
     } else {
@@ -29,12 +32,25 @@ class MeRepository {
     return await storage.delete(key: 'token');
   }
 
+  Future<void> deleteDeviceId() async {
+    return await storage.delete(key: 'deviceId');
+  }
+
   Future<void> persistUserId(String userId) async {
     return await storage.write(key: 'userId', value: userId);
   }
 
   Future<void> persistToken(String token) async {
     return await storage.write(key: 'token', value: token);
+  }
+
+  Future<void> persistDeviceId() async {
+    const String component = "0123456789abcdef";
+    String deviceId = "";
+    for (int i = 0; i < 40; i++) {
+      deviceId += component[(Random().nextDouble() * component.length).floor()];
+    }
+    return await storage.write(key: 'deviceId', value: deviceId);
   }
 
   Future<String> readUserId() async {
@@ -45,6 +61,10 @@ class MeRepository {
     return await storage.read(key: 'token');
   }
 
+  Future<String> readDeviceId() async {
+    return await storage.read(key: 'deviceId');
+  }
+
   Future<bool> hasUserId() async {
     String value = await readUserId();
     return value != null;
@@ -52,6 +72,11 @@ class MeRepository {
 
   Future<bool> hasToken() async {
     String value = await readToken();
+    return value != null;
+  }
+
+  Future<bool> hasDeviceId() async {
+    String value = await readDeviceId();
     return value != null;
   }
 }
