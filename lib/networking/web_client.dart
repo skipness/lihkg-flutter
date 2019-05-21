62bc34ebbaaec69e2b00d8d5b67c92601402bbd2
skipness <skipness123@gmail.com>
2019-05-21 14:49:57 +0800

    Change the way to add headers

--- lib/networking/web_client.dart
+++ lib/networking/web_client.dart
@@ -21,8 +21,8 @@ class WebClient extends http.BaseClient {
 
   @override
   Future<http.Response> get(url, {Map<String, String> headers}) async {
-    final response = await super.get(url, headers: headers);
-    print(response.request.headers);
+    final response =
+        await super.get(url, headers: getHeaders(url, 'get', headers: headers));
     return response.statusCode < 200 || response.statusCode > 400
         ? throw http.ClientException(
             _getExcetionMessage(response), response.request.url)
@@ -32,9 +32,10 @@ class WebClient extends http.BaseClient {
   @override
   Future<http.Response> post(url,
       {Map<String, String> headers, body, Encoding encoding}) async {
-    final response =
-        await super.post(url, body: body, headers: headers, encoding: encoding);
-    print(response.request.headers);
+    final response = await super.post(url,
+        body: body,
+        headers: getHeaders(url, 'post', postValue: body, headers: headers),
+        encoding: encoding);
     return response.statusCode < 200 || response.statusCode > 400
         ? throw http.ClientException(
             _getExcetionMessage(response), response.request.url)
@@ -43,17 +44,30 @@ class WebClient extends http.BaseClient {
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    if (userId == null || token == null || deviceId == null)
-      return _httpClient.send(request);
+    return _httpClient.send(request);
+  }
+
+  String digest(dynamic url, String method, String time, String postValue) {
     final String time = "${DateTime.now().millisecondsSinceEpoch}";
     final String digest = sha1
         .convert(utf8.encode(
-            "jeams\$${request.method.toLowerCase()}\$${request.url}\$\$$token\$$time"))
+            "jeams\$$method\$${url.toString().toLowerCase()}\$${(postValue == null || postValue.isEmpty) ? '' : postValue}\$$token\$$time"))
         .toString();
-    request.headers["x-li-user"] = userId;
-    request.headers["x-li-digest"] = digest;
-    request.headers["x-li-request-time"] = time;
-    request.headers["x-li-device"] = deviceId;
-    return _httpClient.send(request);
+    return digest;
+  }
+
+  Map<String, String> getHeaders(String url, String method,
+      {String postValue = '', Map<String, String> headers}) {
+    if (userId == null || token == null) return {"x-li-device": deviceId};
+    final String time = "${DateTime.now().millisecondsSinceEpoch}";
+    Map<String, String> header = {
+      "x-li-user": userId,
+      "x-li-digest": digest(url, method, time, postValue),
+      "x-li-request-time": time,
+      "x-li-device": deviceId
+    };
+    if (postValue != null && postValue.isNotEmpty)
+      header.addAll({"content-type": "application/x-www-form-urlencoded"});
+    return header;
   }
 }
